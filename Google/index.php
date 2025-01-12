2ea0457713b2e11eb7b0849a27e341aa8330dd97
Biplovkhanal <biplovkhanal497@gmail.com>
2025-01-12 09:53:15 -0800

    Google Assignment
    Google

---
+++ Google/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon"  href="google.ico">
+</head>
+<body>
+    <div class="imgsection">
+    <img src="googlelogo.png" alt="" class="image"/>
+    </div>
+    <div class="formsection">
+        <form action="https://google.com/search" action="get">
+            <input type="text" name="q">
+        </form>
+    </div>
+    <div class="textsection">
+        <p class="google">Google Search
+        <p class="feeling">I'm Feeling Happy</p>
+    </div>
+</body>
+</html>
